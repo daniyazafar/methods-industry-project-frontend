facdacc8a424baaae059544f1215b381d2b7cf9e
tamimp <tamipoliwoda@hotmail.com>
2024-09-12 15:57:14 -0400

    changed header text

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,13 +6,16 @@ import brainstation from "../../assets/logo/brainstation-logo.png";
 const Header = () => {
   return (
     <header className="header">
-      <img className="header__logo" src={telus} alt="Telus Logo" />
-      <h1 className="header__text">Flix Chooser</h1>
-      <img
-        className="header__logo"
-        src={brainstation}
-        alt="BrainStation Logo"
-      />
+      <div className="header__title">
+        <img className="header__logo" src={telus} alt="Telus Logo" />
+        <h1 className="header__text">Stream Team</h1>
+        <img
+          className="header__logo"
+          src={brainstation}
+          alt="BrainStation Logo"
+        />
+      </div>
+      <p>Every person is different. So is every streaming service. </p>
     </header>
   );
 };
